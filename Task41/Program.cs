2c96eb388bb4eade13ad70266743814576711938
Ekyd <Ekyd777@gmail.com>
2022-05-02 20:18:43 +0300

    add correkting task41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,17 +1,12 @@
 ﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 //0, 7, 8, -2, -2 -> 2
 //-1, -7, 567, 89, 223-> 3
-int ImputNumber (string? massiveNumber)
-{
-    Console.Write($"Введите число: ");
-    int number = Convert.ToInt32(Console.ReadLine());
-    return number;
-}
 void Fillrray(int[] array)   
 {
     for(int i = 0; i < array.Length; i++)
     {
-    array[i] = ImputNumber(Console.ReadLine());
+    Console.Write($"Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
     }
 }
 void WriteArray(int[] array) 
